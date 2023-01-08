8bdf2295fd70e070f31fe468050dd137b018ffa4
Deepak Kumar <94438204+Deepak931014@users.noreply.github.com>
2023-01-08 17:56:13 +0530

    Update socket.io link

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-app-rn95.onrender.com",
     methods: ["GET", "POST"],
   },
 });
